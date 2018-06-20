90f6e5c9d4b8fee0f8f3d490e09fb7ca3f4a4d9a
Chandan Kumar <chandanchaudharyrules@gmail.com>
2018-06-21 04:07:29 +0530

    Add insert method with LL rotation

--- avl-tree.js
+++ avl-tree.js
@@ -1,3 +1,10 @@
+const rotationTypes = {
+  LL: 'LL',
+  LR: 'LR',
+  RL: 'RL',
+  RR: 'RR'
+}
+
 class AVLTree {
   constructor() {
     this.root = null;
@@ -49,14 +56,88 @@ function insert (item) {
     return;
   }
 
-  insertBST(node, item);
+  let tempNode = insertBST(node, item);
+  let path = [];
+  while(tempNode.parent !== null) {
+    tempNode.parent.setHeight();
+
+    setRotationType(path, tempNode);
+
+    let balance = tempNode.parent.findBalance();
+    if(balance > 1) {
+      let type = getRotationType(path);
+      if(type === rotationTypes.LL) {
+        this.rotateLL(tempNode);
+      } else if (type === rotationTypes.LR) {
+
+      }
+      break;
+    } else if (balance < -1) {
+      let type = getRotationType(path);
+      if(type === rotationTypes.LR) {
+
+      } else if (type === rotationTypes.RR) {
+
+      }
+      break;
+    }
+    tempNode = tempNode.parent;
+  }
+}
+
+function getRotationType(path) {
+  return path.reverse().join('');
+}
+
+function setRotationType(path, node) {
+  if(node === node.parent.left) {
+    path.push('L');
+  } else {
+    path.push('R');
+  }
+  if(path.length === 3) {
+    path.shift();
+  }
+}
+
+function rotateLL(node) {
+  let parent = node.parent;
+  parent.left = node.right;
+  if (node.right) {
+    node.right.parent = parent;
+  }
+  if (parent.parent) {
+    node.parent = parent.parent;
+    if (parent.parent.left === parent) {
+      parent.parent.left = node;
+    } else {
+      parent.parent.right = node;
+    }
+  } else if (parent === this.root) {
+    this.root = node;
+  }
+  node.right = parent;
+  node.right.setHeight();
+  node.setHeight();
 }
 
 function setHeight() {
   this.height = 1 + Math.max((this.left || 0) && this.left.height, (this.right || 0) && this.right.height);
 }
 
+function findBalance() {
+  if (this.left && this.right) {
+    return this.left.height - this.right.height;
+  } else if (this.left) {
+    return this.left.height;
+  } else if (this.right) {
+    return this.right.height;
+  }
+}
+
 Node.prototype.setHeight = setHeight;
+Node.prototype.findBalance = findBalance;
 AVLTree.prototype.insert = insert;
+AVLTree.prototype.rotateLL = rotateLL;
 
 module.exports = AVLTree;
